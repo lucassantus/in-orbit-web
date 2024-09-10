ba9f24d6544b30cfcf3eb9a78930ca6e1b39e2fb
Lucas Santos <leos.developer@gmail.com>
2024-09-10 20:38:49 -0300

    feat: add monorepo server and web

---
+++ server/src/db/schema.ts
@@ -0,0 +1,15 @@
+import { integer } from 'drizzle-orm/pg-core'
+import { timestamp } from 'drizzle-orm/pg-core'
+import { text } from 'drizzle-orm/pg-core'
+import { pgTable } from 'drizzle-orm/pg-core'
+
+export const goals = pgTable('goals', {
+  id: text('id').primaryKey(),
+  title: text('title').notNull(),
+  desiredWeeklyFrequency: integer('desired_weekly_frequency').notNull(),
+  createdAt: timestamp('created_at', {
+    withTimezone: true,
+  })
+    .notNull()
+    .defaultNow(),
+})
