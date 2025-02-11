b2548baccccedf76cdba71b80afef7674160df5b
Lucas Santos <leos.developer@gmail.com>
2025-02-10 23:12:14 -0300

    feat: create new table users

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,17 +1,26 @@
-import { client, db } from '.'
-import { goalCompletions, goals } from './schema'
 import dayjs from 'dayjs'
+import { client, db } from '.'
+import { goalCompletions, goals, users } from './schema'
 
 async function seed() {
   await db.delete(goalCompletions)
   await db.delete(goals)
 
+  const [user] = await db
+    .insert(users)
+    .values({
+      name: 'John Doe',
+      externalAccountId: 43565,
+      avatarUrl: 'https://github.com/LucasSantus.png',
+    })
+    .returning()
+
   const result = await db
     .insert(goals)
     .values([
-      { title: 'Acordar cedo', desiredWeeklyFrequency: 5 },
-      { title: 'Me exercitar', desiredWeeklyFrequency: 3 },
-      { title: 'Meditar', desiredWeeklyFrequency: 1 },
+      { title: 'Acordar cedo', desiredWeeklyFrequency: 5, userId: user.id },
+      { title: 'Me exercitar', desiredWeeklyFrequency: 3, userId: user.id },
+      { title: 'Meditar', desiredWeeklyFrequency: 1, userId: user.id },
     ])
     .returning()
 
