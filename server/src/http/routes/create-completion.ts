d19c91331c3b136b46147871ccac9b800dd7f19c
Lucas Santos <leos.developer@gmail.com>
2025-02-10 22:48:48 -0300

    feat: documenting routes

--- server/src/http/routes/create-completion.ts
+++ server/src/http/routes/create-completion.ts
@@ -1,5 +1,5 @@
-import { z } from 'zod'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod'
 import { createGoalCompletion } from '../../functions/create-goal-completion'
 
 export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
@@ -7,17 +7,23 @@ export const createCompletionRoute: FastifyPluginAsyncZod = async app => {
     '/completions',
     {
       schema: {
+        tags: ['goals'],
+        description: 'Complete a goal',
         body: z.object({
           goalId: z.string(),
         }),
+        response: {
+          201: z.null(),
+        },
       },
     },
-    async request => {
+    async (request, reply) => {
       const { goalId } = request.body
 
       await createGoalCompletion({
         goalId,
       })
+      return reply.status(201).send()
     }
   )
 }
