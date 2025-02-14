00d3cd962ba59b389eaddf59346794a87363606d
Lucas Santos <leos.developer@gmail.com>
2025-02-13 23:37:02 -0300

    feat: updating code of project

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,10 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import './index.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './app'
-
-import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from 'sonner'
 
 const queryClient = new QueryClient()
 
@@ -11,6 +12,8 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+
+      <Toaster position="bottom-left" richColors />
     </QueryClientProvider>
   </StrictMode>
 )
